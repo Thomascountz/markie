1a281c8da1c370f15811c0157063094864ddb264
thomascountz <thomascountz@gmail.com>
2019-09-10 08:39:38 -0400

    iterate through a list of rules

--- lib/markie/parser.rb
+++ lib/markie/parser.rb
@@ -14,29 +14,49 @@ module Markie
       end
 
       def parse_paragraph(tokens, children = [])
-        if tokens[0].type == :eof
-          return Node.new(type: :paragraph, token_count: children.map(&:token_count).sum, children: children)
+        next_child = nil
 
-        elsif tokens[0].type == :text
-          next_child = Node.new(type: :text, token_count: 1, value: tokens[0].value)
+        paragraph = grammar[:paragraph].call(tokens, children)
+        return paragraph if paragraph
 
-        elsif tokens[0].type == :underscore && tokens[1].type == :text && tokens[2].type == :underscore
-          next_child = Node.new(type: :emphasis, token_count: 3, value: tokens[1].value)
-
-        elsif tokens[0].type == :asterisk && tokens[1].type == :text && tokens[2].type == :asterisk
-          next_child = Node.new(type: :emphasis, token_count: 3, value: tokens[1].value)
-
-        elsif tokens[0].type == :open_square_bracket && tokens[1].type == :text && tokens[2].type == :close_square_bracket && tokens[3].type == :open_parenthesis && tokens[4].type == :text && tokens[5].type == :close_parenthesis
-          next_child = Node.new(type: :link, token_count: 6, value: tokens[4].value, children: [
-            Node.new(type: :text, token_count: 1, value: tokens[1].value),
-          ])
-
+        grammar.values_at(:text, :emphasis, :link).each do |grammar|
+          next_child = grammar.call(tokens, children)
+          break if next_child
         end
 
         children.append(next_child)
         next_tokens = tokens[next_child.token_count..-1]
 
         parse_paragraph(next_tokens, children)
+      end
+
+      def grammar
+        {
+          paragraph: ->(tokens, children) {
+            if tokens[0].type == :eof
+              Node.new(type: :paragraph, token_count: children.map(&:token_count).sum, children: children)
+            end
+          },
+          text: ->(tokens, children) {
+            if tokens[0].type == :text
+              Node.new(type: :text, token_count: 1, value: tokens[0].value)
+            end
+          },
+          emphasis: ->(tokens, children) {
+            if
+              tokens[0].type == :underscore && tokens[1].type == :text && tokens[2].type == :underscore ||
+                  tokens[0].type == :asterisk && tokens[1].type == :text && tokens[2].type == :asterisk
+              Node.new(type: :emphasis, token_count: 3, value: tokens[1].value)
+            end
+          },
+          link: ->(tokens, children) {
+            if tokens[0].type == :open_square_bracket && tokens[1].type == :text && tokens[2].type == :close_square_bracket && tokens[3].type == :open_parenthesis && tokens[4].type == :text && tokens[5].type == :close_parenthesis
+              Node.new(type: :link, token_count: 6, value: tokens[4].value, children: [
+                grammar[:text].call(tokens[1..-1], children)
+              ])
+            end
+          },
+        }
       end
     end
   end
